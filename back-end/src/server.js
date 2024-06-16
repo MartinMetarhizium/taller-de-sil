33ecacf86c1264f4020984a20d56fe8f56f11035
MartinMetarhizium <martin.horn2014@gmail.com>
2024-06-16 18:05:47 -0300

    initial commit

---
+++ back-end/src/server.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+import cors from 'cors';
+
+async function start() {
+    const url = `mongodb+srv://martinhorn2014:cZzeQcltSuxgvfTX@vue-project.kodctjv.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(url);
+
+    const app = express();
+    app.use(express.json());
+    app.use(cors()); // Enable CORS for all routes
+
+    // Serve static files
+    app.use('/images', express.static(path.join(__dirname, '../assets')));
+
+    await client.connect();
+    const db = client.db('fsv-db');
+
+    app.get('/api/products', async (req, res) => {
+        const products = await db.collection('products').find({}).toArray();
+        res.send(products);
+    });
+
+    async function populatedCartIds(ids) {
+        return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
+    }
+
+    app.get('/api/users/:userId/cart', async (req, res) => {
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        if (user && user?.cartItems || []) {
+            const populatedCart = await populatedCartIds(user?.cartItems || []);
+            res.json(populatedCart);
+        } else {
+            res.status(404).json({ error: 'User not found or cart is empty' });
+        }
+    });
+
+    app.post('/api/users/:userId/cart', async (req, res) => {
+        const userId = req.params.userId;
+        const productId = req.body.id;
+
+        const existingUser = await db.collection('users').findOne({id:userId});
+
+        if (!existingUser) {
+            await db.collection('users').insertOne({id:userId, cartItems: []});
+        }
+
+        await db.collection('users').updateOne({ id: userId }, {
+            $addToSet: { cartItems: productId }
+        });
+
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populatedCartIds(user?.cartItems || []);
+        res.json(populatedCart);
+    });
+
+    app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
+        const userId = req.params.userId;
+        const productId = req.params.productId;
+
+        await db.collection('users').updateOne({ id: userId }, {
+            $pull: { cartItems: productId }
+        });
+
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populatedCartIds(user?.cartItems || []);
+        res.json(populatedCart);
+    });
+
+    app.get('/api/products/:productId', async (req, res) => {
+        const productId = req.params.productId;
+        const product = await db.collection('products').findOne({ id: productId });
+        res.json(product);
+    });
+
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
+}
+
+start();
---
+++ back-end/src/temp-data.js
@@ -0,0 +1,73 @@
+// import basilBottle from './assets/basil-bottle.webp';
+// import lemonBottle from './assets/lemon-bottle.webp';
+// import garlicBottle from './assets/garlic-bottle.webp';
+// import rosemaryBottle from './assets/rosemary-bottle.webp';
+// import chiliBottle from './assets/chili-bottle.webp';
+// import delicateBottle from './assets/delicate-bottle.webp';
+// import mediumBottle from './assets/medium-bottle.webp';
+// import boldBottle from './assets/bold-bottle.webp';
+// luna.jpg
+
+
+export const cartItems = [ '123', '345' ];
+
+export const products = [{
+    id: '123',
+    name: 'Basil',
+    price: '$26.00',
+    imageUrl: '/images/basil-bottle.webp'
+}, {
+    id: '234',
+    name: 'Lemon',
+    price: '$26.00',
+    imageUrl: '/images/lemon-bottle.webp'
+}, {
+    id: '345',
+    name: 'Garlic',
+    price: '$26.00',
+    imageUrl: '/images/garlic-bottle.webp'
+}, {
+    id: '456',
+    name: 'Rosemary',
+    price: '$26.00',
+    imageUrl: '/images/rosemary-bottle.webp'
+}, {
+    id: '567',
+    name: 'Chili',
+    price: '$26.00',
+    imageUrl: '/images/chili-bottle.webp'
+}, {
+    id: '678',
+    name: 'Delicate',
+    price: '$24.50',
+    imageUrl: '/images/delicate-bottle.webp'
+}, {
+    id: '1',
+    name: 'Perra negra y fea',
+    price: '$0',
+    imageUrl: '/images/luna.jpg'
+}, { 
+    id: '2',
+    name: 'Negrito sin dientes',
+    price: '$0',
+    imageUrl: '/images/negro-sin-dientes.jpg'
+},
+{ 
+    id: '3',
+    name: 'Duolingo de la salada',
+    price: '$5',
+    imageUrl: '/images/duolingo.jpg'
+},
+{ 
+    id: '4',
+    name: 'Jesus que pagaba el gimnasio',
+    price: '$12',
+    imageUrl: '/images/jesus.jpg'
+},
+{ 
+    id: '5',
+    name: 'Axel kicillof',
+    price: '$7',
+    imageUrl: '/images/duende1.jpg'
+}
+];
